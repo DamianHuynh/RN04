083ab08e06b3f4e300bd0dd563e3dfab92dd6f7b
Dat Tuan Huynh <huynhtuandat2797@gmail.com>
2022-02-14 21:11:40 +0700

    Hanlde Logic for Game App

--- src/GameApp/components/PlayerSelect.js
+++ src/GameApp/components/PlayerSelect.js
@@ -1,13 +1,22 @@
-import {View, StyleSheet, Image} from 'react-native';
+import {View, StyleSheet, Image, TouchableOpacity} from 'react-native';
 import React, {Component} from 'react';
+import {Scissor} from '../assets';
 
 export default class PlayerSelect extends Component {
   render() {
-    const {image} = this.props;
+    const {selectOption, onPress, selectedOption} = this.props;
+
+    // {type:'string', image}
+
     return (
-      <View style={styles.selectItemContainer}>
-        <Image source={image} style={styles.selectItem} />
-      </View>
+      <TouchableOpacity
+        style={[
+          styles.selectItemContainer,
+          selectedOption === selectOption.type && styles.selectedItem,
+        ]}
+        onPress={() => onPress(selectOption)}>
+        <Image source={selectOption.image} style={styles.selectItem} />
+      </TouchableOpacity>
     );
   }
 }
@@ -17,8 +26,6 @@ const styles = StyleSheet.create({
     height: 80,
     width: 80,
     backgroundColor: 'rgba(0,0,0,0.6)',
-    borderColor: '#c0bf2c',
-    borderWidth: 3,
     borderRadius: 8,
     justifyContent: 'center',
     alignItems: 'center',
@@ -26,5 +33,9 @@ const styles = StyleSheet.create({
   selectItem: {
     height: 60,
     width: 60,
+  },
+  selectedItem: {
+    borderColor: '#c0bf2c',
+    borderWidth: 3,
   },
 });
